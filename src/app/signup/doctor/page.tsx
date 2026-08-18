35c97144760082d36db38eeca4d98eb1d5ffdae9
YvHarshit <yvharshit@gmail.com>
2026-08-18 18:19:58 +0530

    Only created page file for H.Admin & Doctor SignUp

--- src/app/signup/doctor/page.tsx
+++ src/app/signup/doctor/page.tsx
@@ -1,40 +1,12 @@
 "use client";
 
-
-import { LocalHospital } from "@mui/icons-material";
-import { useRouter } from "next/navigation";
-import axios from "axios"
 import AuthSideImage from "@/shared/authSharedImg";
-import { LoginFormData } from "@/validations/loginData";
+import { LocalHospital } from "@mui/icons-material";
 
 export default function hospitalAdminLogin() {
-  const router = useRouter();
-
-  const handleSignup = async (data: LoginFormData) => {
-    try {
-      const response = await axios.post("http://localhost:3000/api/auth/doctor/login", data)
-
-      console.log(response.data);
-      if(response.data.success){
-        console.log(response.data.message)
-        router.push("/doctor");
-      }
-      else console.log("Something wrong")
-
-     
-      
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        console.log(error.response?.data?.message);
-      } else {
-        console.log("Something went wrong");
-      }
-    }
-  };
   return (
     <div className="flex">
       
-
       <div className="flex-1 flex flex-col justify-center items-center">
         <div className="my-9 block lg:hidden">
           <h2 className="text-3xl font-bold flex gap-4 items-center">
@@ -43,8 +15,8 @@ export default function hospitalAdminLogin() {
           </h2>
           <p className="text-lg text-center"> Access Your Centralized Hospital Admin Portal  </p>
         </div>
-
-        <div className="text-7xl">  sign up </div>
+        
+        <div className="text-7xl">  Doctor sign up </div>
       </div>
 
 
--- src/app/signup/hospitalAdmin/page.tsx
+++ src/app/signup/hospitalAdmin/page.tsx
@@ -1,54 +1,28 @@
-"use client"
+"use client";
 
 import AuthSideImage from "@/shared/authSharedImg";
-import LoginForm from "@/shared/LoginForm";
-import { LoginFormData } from "@/validations/loginData";
 import { LocalHospital } from "@mui/icons-material";
-import axios from "axios";
-import { useRouter } from "next/navigation";
 
 export default function hospitalAdminLogin() {
-    const router = useRouter()
-     const handleLogin = async (data: LoginFormData) => {
-    try {
-      const response = await axios.post("http://localhost:3000/api/auth/hospital-admin/login", data)
+  return (
+    <div className="flex">
 
-      console.log(response.data);
-      if(response.data.success){
-        console.log(response.data.message)
-        router.push("/hospitalAdmin");
-      }
-      else console.log("Something wrong")
-
-      // Login successful
-      
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        console.log(error.response?.data?.message);
-      } else {
-        console.log("Something went wrong");
-      }
-    }
-  };
-    return (
-       
-
-
-            <div className="flex-1 flex flex-col justify-center items-center">
-            <div className="my-9 block lg:hidden">
-                <h2 className="text-3xl font-bold flex gap-4 items-center"> 
-                 <LocalHospital fontSize="large" className="text-blue-700 bg-white rounded text-center"/> City General Hospital 
-                </h2>
-                <p className="text-lg text-center"> Access Your Centralized Hospital Admin Portal</p>
-            </div>
-            <div>    Hospital admin sign up     </div>
-              
-
-             <div className="flex">
-            <div className="relative flex-1 w-max min-h-screen hidden lg:block">
-               <AuthSideImage/>
-            </div>
+      <div className="flex-1 flex flex-col justify-center items-center">
+        <div className="my-9 block lg:hidden">
+          <h2 className="text-3xl font-bold flex gap-4 items-center">
+            <LocalHospital fontSize="large" className="text-blue-700 bg-white rounded text-center"/>
+            City General Hospital
+          </h2>
+          <p className="text-lg text-center">
+            Access Your Centralized Hospital Admin Portal
+          </p>
         </div>
+        <div className=" text-7xl"> Hospital admin sign up </div>
       </div>
-    );
+
+      <div className="relative flex-1 w-max min-h-screen hidden lg:block">
+        <AuthSideImage />
+      </div>
+    </div>
+  );
 }
